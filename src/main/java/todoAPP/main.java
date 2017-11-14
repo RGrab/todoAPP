390bd77d1f92f1ca1901ffc82d70ee083fb41fd1
RGrab <rgrabar@csumb.edu>
2017-11-13 17:49:47 -0800

    complete menuFactory with logging

--- src/main/java/todoAPP/main.java
+++ src/main/java/todoAPP/main.java
@@ -7,7 +7,9 @@ public class main {
   static DatabaseConnection connection;
 
   public static void main(String[] args){
+
     connection = new DatabaseConnection("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/todoAPP?useSSL=false", "user", "password");
-    System.out.println("DONE!");
+
+
   }
 }
